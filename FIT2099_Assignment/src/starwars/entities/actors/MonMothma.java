0faf270dd7dbc6e33d2f9a0f97ed1effd525fb26
Seolhyun95 <darylhcw@gmail.com>
2017-05-24 01:10:44 +0800

    Add files via upload
    Admiral Ackbar and Mon Mothma Classes

--- FIT2099_Assignment/src/starwars/entities/actors/MonMothma.java
+++ FIT2099_Assignment/src/starwars/entities/actors/MonMothma.java
@@ -8,42 +8,49 @@ import starwars.SWWorld;
 import starwars.Team;
 import starwars.actions.MindControl;
 
+
+
 /**
- * Admiral Ackbar
+ * MonMothma
  * 
- * Admiral Ackbar is similar to a Dummy.
- * Does not really do anything but stand still and say "It's a trap"
+ * Admiral Mothma is similar to a Dummy.
+ * Does not really do anything but stand still and say things
  * No attacking in act() because there really is no way to bring any enemy to the rebel base
- * OR bring Ackbar out of the base.
+ * OR bring Mothma out of the base.
  * Is a Legend because you can only make one.
  * Weak-minded and can be mindcontrolled. (Don't feel that this is important though)
  * 
  * @Daryl Ho
  *
  */
-public class AdmiralAckbar extends SWLegend {
+public class MonMothma extends SWLegend {
 
-	private static AdmiralAckbar ackbar = null; 
+	private static MonMothma mothma = null; 
 	
-	private AdmiralAckbar(MessageRenderer m, SWWorld world) {
+	private MonMothma(MessageRenderer m, SWWorld world) {
 		super(Team.GOOD, 200, 1, m, world);
-		this.setShortDescription("Admiral Ackbar");
-		this.setLongDescription("Admiral Ackbar, the Mon Calamari military commander of the rebel alliance");
+		this.setShortDescription("Mon Mothma");
+		this.setLongDescription("Mon Mothma, a prominent figure for the Rebel Alliance");
 		SWAffordance mindcontrol = new MindControl(this,m);
 		this.addAffordance(mindcontrol);
 	}
 
-	public static AdmiralAckbar getAdmiralAckbar(MessageRenderer m, SWWorld world) {
-		ackbar = new AdmiralAckbar(m, world);
-		ackbar.activate();
-		return ackbar;
+	public static MonMothma getMonMothma(MessageRenderer m, SWWorld world) {
+		mothma = new MonMothma(m, world);
+		mothma.activate();
+		return mothma;
 	}
 	
 	private String describeLocation() {
 		SWLocation location = this.world.getEntityManager().whereIs(this);
 		return this.getShortDescription() + " [" + this.getHitpoints() + "] is at " + location.getShortDescription();
-
 	}
+	
+	private void sayFarmboy(){
+	// Function to say her line, triggered when Luke TRAVELS to Rebel HQ without the right team
+		say("What are you doing here, farmboy? Bring us General Organa and the plans!");
+	}
+	
 	@Override
 	protected void legendAct() {
 		
@@ -51,9 +58,8 @@ public class AdmiralAckbar extends SWLegend {
 			return;
 		}
 		say(describeLocation());
-		if (Math.random() > 0.9){
-			say(getShortDescription() + "says" + "\"It's a trap!\"");
-		}
+		
 	}
 }
+
 
